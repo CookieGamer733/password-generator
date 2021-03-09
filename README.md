dacc4f8e2075874ce9146e42e94c6052b3afff31
CookieGamer733 <74946768+CookieGamer733@users.noreply.github.com>
2021-03-09 14:42:38 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Password Generator
+A simple password generator made with Python
+
+NOTE: This version has a command to change the title of the prompt on Windows. If you use on something that isn't Windows, it might not work.
+
+### Required Modules
+ - random
+ - pyperclip
---
+++ genpass.py
@@ -0,0 +1,16 @@
+from os import system
+system("title Password Generator")
+
+import random
+import pyperclip
+
+password = ""
+length = int(input("Length: "))
+
+for i in range(0, length):
+  password += chr(random.randint(33, 126))
+
+print("Password: " + password)
+
+if (input("Copy to clipboard (Y/N)? ")) in ["Y","y"]:
+  pyperclip.copy(password)
